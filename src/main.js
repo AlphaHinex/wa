f49373eefbfcedfc359b73ce5013e7f7e8489a1e
AlphaHinex <AlphaHinex@gmail.com>
2015-11-22 01:05:43 +0800

    Case page looks good now

--- src/main.js
+++ src/main.js
@@ -2,19 +2,44 @@
 
 var app = angular.module('wa', ['ngRoute', 'ngMaterial', 'md.data.table']);
 
+var allInsurances = function() {
+  var all = '大地保险, 中华财险, 永安保险, ' +
+            'PICC, 太平洋保险, 大都会, 人保财险';
+  return all.split(/, +/g).map(function(ins) {
+    return ins;
+  });
+};
+
+var createFilterFor = function(query) {
+  return function filterFn(ins) {
+    return ins.indexOf(query) === 0;
+  };
+};
+
+var newCase = function() {
+  return {
+    initDate: new Date()
+  };
+};
+
 var appCtrl = function($scope) {
-  $scope.case = {
-    date: new Date()
+  var self = this;
+  self.insurances = allInsurances();
+  self.querySearch = function(query) {
+    return query ? self.insurances.filter(createFilterFor(query)) : self.insurances;
   };
 
+  $scope.case = newCase();
+
   $scope.save = function() {
+    if (!$scope.case.insurance) {
+      $scope.case.insurance = self.searchText;
+    }
     console.log($scope.case);
   };
 
   $scope.cancel = function() {
-    $scope.case = {
-      date: new Date()
-    };
+    $scope.case = newCase();
   };
 };
 
