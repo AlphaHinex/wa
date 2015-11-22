f942262e22a11290c70394e5f14391f2e3865b50
AlphaHinex <AlphaHinex@gmail.com>
2015-11-22 19:48:26 +0800

    Record case feature

--- src/main.js
+++ src/main.js
@@ -5,23 +5,24 @@ var Case = AV.Object.extend('Case');
 
 var app = angular.module('wa', ['ngRoute', 'ngMaterial', 'md.data.table']);
 
-var allInsurances = function() {
+var allDefendants = function() {
   var all = '人保财险, 平安保险, 天安保险, 中华联合, 人寿财险, ' +
             '浙商保险, 华泰保险, 民安保险, 永安保险, 大地保险, ' +
             '永诚保险, 都邦保险, 信达保险, 华安保险, 太平保险, ' +
             '太平洋保险, 安华农业, 紫金财险, 渤海保险, 中银保险';
-  return all.split(/, +/g).map(function(ins) {
-    return ins;
+  return all.split(/, +/g).map(function(item) {
+    return item;
   });
 };
 
 var createFilterFor = function(query) {
-  return function filterFn(ins) {
-    return ins.indexOf(query) === 0;
+  return function filterFn(item) {
+    return item.indexOf(query) === 0;
   };
 };
 
-var newCase = function() {
+var newCase = function(ctrl) {
+  ctrl.searchText = '';
   return {
     initDate: new Date()
   };
@@ -34,23 +35,23 @@ var allStates = function() {
 var appCtrl = function($scope, $mdToast) {
   var self = this;
   self.states = allStates();
-  self.insurances = allInsurances();
+  self.allDefendants = allDefendants();
   self.querySearch = function(query) {
-    return query ? self.insurances.filter(createFilterFor(query)) : self.insurances;
+    return query ? self.allDefendants.filter(createFilterFor(query)) : self.allDefendants;
   };
 
-  $scope.case = newCase();
+  $scope.case = newCase(self);
 
   $scope.save = function() {
-    if (!$scope.case.insurance) {
-      $scope.case.insurance = self.searchText;
+    if (!$scope.case.defendants) {
+      $scope.case.defendants = self.searchText;
     }
     console.log($scope.case);
     var c = Case.new($scope.case);
     c.save(null, {
       success: function(c) {
         console.log('Save case with objectId: ' + c.id);
-        $scope.case = newCase();
+        $scope.case = newCase(self);
         $mdToast.show(
           $mdToast.simple()
             .content('保存成功!')
@@ -59,13 +60,13 @@ var appCtrl = function($scope, $mdToast) {
         );
       },
       error: function(c, error) {
-      console.log('Save case failed cause: ' + error.message);
+        console.log('Save case failed cause: ' + error.message);
       }
     });
   };
 
   $scope.cancel = function() {
-    $scope.case = newCase();
+    $scope.case = newCase(self);
   };
 };
 
