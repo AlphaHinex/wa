707e49f062c6111505aa93db78517622046b8e45
AlphaHinex <AlphaHinex@gmail.com>
2015-11-24 14:47:57 +0800

    Should partly match defendants

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,8 @@ var allDefendants = function() {
   var all = '人保财险, 平安保险, 天安保险, 中华联合, 人寿财险, ' +
             '浙商保险, 华泰保险, 民安保险, 永安保险, 大地保险, ' +
             '永诚保险, 都邦保险, 信达保险, 华安保险, 太平保险, ' +
-            '太平洋保险, 安华农业, 紫金财险, 渤海保险, 中银保险';
+            '太平洋保险, 安华农业, 紫金财险, 渤海保险, 中银保险, ' +
+            '英大泰和保险';
   return all.split(/, +/g).map(function(item) {
     return item;
   });
@@ -17,7 +18,7 @@ var allDefendants = function() {
 
 var createFilterFor = function(query) {
   return function filterFn(item) {
-    return item.indexOf(query) === 0;
+    return item.indexOf(query) !== -1;
   };
 };
 
