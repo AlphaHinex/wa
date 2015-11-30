e976e188d2ab6a6d54698394e20fd360d3bf3d1a
AlphaHinex <AlphaHinex@gmail.com>
2015-11-30 20:06:28 +0800

    Change template module name

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'templateApp',
+          module: 'wa',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
